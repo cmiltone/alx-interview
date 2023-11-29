d378a340b242618b5253208d5f1d700ccea61341
Castro Hamiltone <castromiltone@gmail.com>
2023-11-29 17:38:27 +0300

    codestyle fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,13 +4,13 @@ from math import factorial
 
 
 def pascal_triangle(n):
-   """generates intergers for pascal's triangle"""
+    """generates intergers for pascal's triangle"""
 
-   arr = []
-   n = 5
-   for i in range(n):
-       arr.append([])
-       for j in range(i+1):
-           arr[i].append(factorial(i)//(factorial(j)*factorial(i-j)))
+    arr = []
+    n = 5
+    for i in range(n):
+        arr.append([])
+        for j in range(i+1):
+            arr[i].append(factorial(i)//(factorial(j)*factorial(i-j)))
 
-   return arr
+    return arr
