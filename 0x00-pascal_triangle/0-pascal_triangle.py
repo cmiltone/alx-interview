3c62f262fd4c359084c2f1a1f3d02967ab97566e
Castro Hamiltone <castromiltone@gmail.com>
2023-11-29 17:52:24 +0300

    fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,6 +2,13 @@
 """module defnes function that generates
 pascal's triangle integer"""
 
+def factorial(n):
+    fact = 1
+
+    for i in range(1, n+1):
+        fact = fact * i
+
+    return fact
 
 def pascal_triangle(n):
     """generates intergers for pascal's triangle"""
@@ -9,6 +16,6 @@ def pascal_triangle(n):
     for i in range(n):
         arr.append([])
         for j in range(i+1):
-            arr[i].append(0)
+            arr[i].append(factorial(i)//(factorial(j)*factorial(i-j)))
 
     return arr
