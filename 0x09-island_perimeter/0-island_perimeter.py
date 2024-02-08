fc416cba2499c6e0b79166bbc695f1e137a20ecd
Castro Hamiltone <castromiltone@gmail.com>
2024-02-08 19:55:37 +0300

    0-island_perimeter.py

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""module declares a function  that returns the return_val of
+the island described in grid:
+
+grid is a list of list of integers:
+0 represents water
+1 represents land
+Each cell is square, with a side length of 1
+Cells are connected horizontally/vertically (not diagonally).
+grid is rectangular, with its width and height not exceeding 100
+The grid is completely surrounded by water
+There is only one island (or nothing).
+The island doesn't have “lakes” (water inside that isn't
+connected to the water surrounding the island).
+"""
+
+
+def island_perimeter(grid):
+    """gets perimeter
+    """
+    return_val = 0
+    if type(grid) != list:
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            boundary = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            return_val += sum(boundary)
+    return return_val
